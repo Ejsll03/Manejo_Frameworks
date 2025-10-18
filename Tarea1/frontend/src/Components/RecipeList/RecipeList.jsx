d6633f080f30ed266d266c8e137ad47a11672527
Ejempr03 <erick.31085525@uru.edu>
2025-10-18 11:54:39 -0400

    Update RecipeList.jsx

--- Tarea1/frontend/src/Components/RecipeList/RecipeList.jsx
+++ Tarea1/frontend/src/Components/RecipeList/RecipeList.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./RecipeList.css";
+import Button from "../Button/Button";
 
-export default function RecipeList({ recipes = [] }) {
+export default function RecipeList({ recipes = [], onView, onEdit, onDelete }) {
   if (!recipes.length) return <div>No hay recetas.</div>;
   return (
     <ul className="recipe-list">
       {recipes.map((recipe) => (
-        <li key={recipe.id} className="recipe-list-item">
+        <li key={recipe._id || recipe.id} className="recipe-list-item">
           <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
+          <div style={{ display: "flex", gap: 8, marginTop: 8 }}>
+            <Button icon="pi pi-eye" onClick={() => onView && onView(recipe)} />
+            <Button
+              icon="pi pi-pencil"
+              onClick={() => onEdit && onEdit(recipe)}
+            />
+            <Button
+              icon="pi pi-trash"
+              className="p-button-danger"
+              onClick={() => onDelete && onDelete(recipe)}
+            />
+          </div>
         </li>
       ))}
     </ul>
